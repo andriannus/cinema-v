48b596e179306e3b70e56f73763df473e8846653
Andriannus Parasian <andriannus.parasian@efishery.com>
2025-02-05 22:09:32 +0700

    chore: setup Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+  css: ["@/assets/css/main.css"],
   devtools: {
     enabled: true,
   },
   srcDir: "src/",
   pages: true,
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
