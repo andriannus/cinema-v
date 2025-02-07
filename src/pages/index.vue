8f57094dd93fdd9685fc829d359977924cdc1833
Andriannus Parasian <andriannus.parasian@efishery.com>
2025-02-07 15:41:11 +0700

    feat: init main content section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,4 +6,25 @@ useHead({
 
 <template>
   <AppHeader />
+
+  <main class="Main">
+    <div class="Main-layout">
+      <p>Hello World</p>
+    </div>
+  </main>
 </template>
+
+<style scoped>
+.Main {
+  @apply flex justify-center;
+  min-height: calc(100vh - 66px - 160px);
+}
+
+.Main-layout {
+  @apply flex;
+
+  max-width: 1280px;
+  padding: 0 16px;
+  width: 100%;
+}
+</style>
